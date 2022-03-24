1391e976d04b7bc425e24797ba778dbe097afd36
zffforgithub <zffforgithub@gmail.com>
2022-03-24 11:26:03 +0800

    mod print

--- src/demo.cpp
+++ src/demo.cpp
@@ -8,12 +8,12 @@ int main()
     int result = sqlite3_open_v2(path, &sql, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE | SQLITE_OPEN_NOMUTEX | SQLITE_OPEN_SHAREDCACHE, NULL);
     if (result == SQLITE_OK)
     {
-        std::clog << "打开数据库连接成功";
+        std::clog << "打开数据库连接成功\n";
         //   std::cout << "Hello CMake!" << std::endl;
     }
     else
     {
-        std::clog << "打开数据库连接失败";
+        std::clog << "打开数据库连接失败\n";
     }
 
     const char *sqlSentence = "INSERT INTO t_log(log_data) VALUES('test1'); "; // SQL语句
@@ -23,13 +23,13 @@ int main()
     int result1 = sqlite3_prepare_v2(sql, sqlSentence, -1, &stmt, NULL);
     if (result1 == SQLITE_OK)
     {
-        std::clog << "添加数据语句OK";
+        std::clog << "添加数据语句OK\n";
         //执行该语句
         sqlite3_step(stmt);
     }
     else
     {
-        std::clog << "添加数据语句有问题";
+        std::clog << "添加数据语句有问题\n";
     }
     //清理语句句柄，准备执行下一个语句
     sqlite3_finalize(stmt);
@@ -41,7 +41,7 @@ int main()
     int result2 = sqlite3_prepare_v2(sql, sqlSentence1, -1, &stmt1, NULL);
     if (result2 == SQLITE_OK)
     {
-        std::clog << "查询语句OK";
+        std::clog << "查询语句OK\n";
         // 每调一次sqlite3_step()函数，stmt语句句柄就会指向下一条记录
         while (sqlite3_step(stmt1) == SQLITE_ROW)
         {
@@ -55,7 +55,7 @@ int main()
     }
     else
     {
-        std::clog << "查询语句有问题";
+        std::clog << "查询语句有问题\n";
     }
     //清理语句句柄，准备执行下一个语句
     sqlite3_finalize(stmt1);
