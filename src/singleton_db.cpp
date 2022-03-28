2987a3d99364390d731219bc05dda75d2ee925f2
zffforgithub <zffforgithub@gmail.com>
2022-03-28 16:24:26 +0800

    add hungry single

--- src/singleton_db.cpp
+++ src/singleton_db.cpp
@@ -1,5 +1,29 @@
 #include "db.hpp"
 
+// lazy single
+// class DbSingleton
+// {
+// private:
+//     DbSingleton(){};
+//     static DbTool *instance;
+
+// public:
+//     static DbTool *GetInstance();
+// };
+// DbTool *DbSingleton::instance = nullptr;
+// DbTool *DbSingleton::GetInstance(/* args */)
+// {
+//     std::mutex mt;
+//     mt.lock();
+//     if (instance == NULL)
+//     {
+//         instance = new DbTool();
+//     }
+//     mt.unlock();
+//     return instance;
+// }
+
+// hungry single -- recommend for multi threads
 class DbSingleton
 {
 private:
@@ -9,19 +33,12 @@ private:
 public:
     static DbTool *GetInstance();
 };
-
+DbTool *DbSingleton::instance = new DbTool();
 DbTool *DbSingleton::GetInstance(/* args */)
 {
-    std::mutex mt;
-    mt.lock();
-    if (instance == NULL)
-    {
-        instance = new DbTool();
-    }
-    mt.unlock();
     return instance;
 }
-DbTool *DbSingleton::instance = NULL;
+
 int main()
 {
     static DbTool *inst = DbSingleton::GetInstance();
